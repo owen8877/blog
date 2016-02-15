22710cd560a9cef931fbc4019e746deb9f7b79f5
owen8877 <xdroid@pku.edu.cn>
2016-02-15 09:08:16 +0800

    Update makepage.sh
    make a page when and only when the source files are modified.

--- generate/makepage.sh
+++ generate/makepage.sh
@@ -4,7 +4,12 @@ do
 	cd ~/gh-pages/blog
 	dir=`echo $1 | awk -F '/' '{print $1"/"$2}'`
 	cd "$dir"
+	shift
 	pwd
+	if [ index.html -nt head_title.shtm ]; then continue; fi
+	if [ index.html -nt body_header.shtm ]; then continue; fi
+	if [ index.html -nt body_section.shtm ]; then continue; fi
+	if [ index.html -nt footer.shtm ]; then continue; fi
 	sed ../../template/article.html -e '/<!--title-->/r head_title.shtm' -e '/<!--header-->/r body_header.shtm' -e '/<!--section-->/r body_section.shtm' -e '/<!--footer-->/r footer.shtm' > index.html
 	if [ $? -eq 0 ]
 	then
@@ -12,5 +17,4 @@ do
 	else
 		echo "Failed!"
 	fi
-	shift
 done
