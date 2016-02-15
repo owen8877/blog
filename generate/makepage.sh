6ef127891693cca3f5710ed6657c3c968edbf83d
owen8877 <xdroid@pku.edu.cn>
2016-02-15 09:36:32 +0800

    Fix generate/makepage.sh

--- generate/makepage.sh
+++ generate/makepage.sh
@@ -6,10 +6,14 @@ do
 	cd "$dir"
 	shift
 	pwd
-	if [ index.html -nt head_title.shtm ]; then continue; fi
-	if [ index.html -nt body_header.shtm ]; then continue; fi
-	if [ index.html -nt body_section.shtm ]; then continue; fi
-	if [ index.html -nt footer.shtm ]; then continue; fi
+	if [ index.html -nt head_title.shtm ]; then
+		if [ index.html -nt body_header.shtm ]; then
+			if [ index.html -nt body_section.shtm ]; then
+				if [ index.html -nt footer.shtm ]; then continue
+				fi
+			fi
+		fi
+	fi
 	sed ../../template/article.html -e '/<!--title-->/r head_title.shtm' -e '/<!--header-->/r body_header.shtm' -e '/<!--section-->/r body_section.shtm' -e '/<!--footer-->/r footer.shtm' > index.html
 	if [ $? -eq 0 ]
 	then
